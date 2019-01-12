b086b06bdcf02b32f7aaeaa30d07d79d791578c5
palaz <barnabas.palatinus@gmail.com>
2019-01-12 16:10:18 +0100

    contact page (WIP)

--- page-contact.php
+++ page-contact.php
@@ -8,16 +8,34 @@ get_header(); ?>
    <div class="container">
       <div class="columns">
          <div class="column">
+            <div class="form-wrap">
+               <?php echo do_shortcode('[contact-form-7 id="210" title="kapcsolat"]'); ?>
+            </div>            
          </div>
          <div class="column">
-            <p>Hun-Trade Kft</p>
-            <p><?php the_field('adress'); ?></p>
-            <p><?php the_field('phone'); ?></p>
-            <p><?php the_field('email'); ?></p>
+            <div class="map"></div>
+            <div>
+               <p class="is-flex is-aligncenter"> 
+                  <span>
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/images/marker.png" alt="marker">
+                  </span> 
+                  <strong>Hun-Trade Kft</strong> <?php the_field('adress'); ?>
+               </p>
+               <p class="is-flex is-aligncenter">
+                  <span>
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/images/phone.png" alt="marker">
+                  </span> 
+                  <?php the_field('phone'); ?>
+               </p>
+               <p class="is-flex is-aligncenter">
+                  <span>
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/images/envelope.png" alt="marker">
+                  </span> 
+                  <?php the_field('email'); ?>
+               </p>
+            </div>            
          </div>
-      </div>
-
-      <?php echo do_shortcode('[contact-form-7 id="210" title="kapcsolat"]'); ?>
+      </div>      
    </div>
 </section>
 <?php get_footer(); ?>
