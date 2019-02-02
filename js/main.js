0605ad122ef323fa4e6feeadb9522963dcfd7716
palaz <barnabas.palatinus@gmail.com>
2019-02-02 15:48:11 +0100

    navigation fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
    $('.main-slider-wrap').slick({
-      preletrow: '<button class="slick-prev"><img src="wp-content/themes/zabszolut/assets/images/chevron.svg"></img></button>',
+      prevArrow: '<button class="slick-prev"><img src="wp-content/themes/zabszolut/assets/images/chevron.svg"></img></button>',
       nextArrow: '<button class="slick-next"><img src="wp-content/themes/zabszolut/assets/images/chevron.svg"></img></button>',
       dots: true,
       autoplay: true,
@@ -19,8 +19,7 @@ $(document).ready(function(){
       arrows: false,
       slidesToShow: 4,
       slidesToScroll: 4,
-      responsive: [
-         {
+      responsive: [{
             breakpoint: 800,
             settings: {
                slidesToShow: 2,
@@ -39,56 +38,58 @@ $(document).ready(function(){
 });
 
 // Mobile menu
-$(document).ready(function() {
-   $(".navbar-burger").click(function() {
-         $(".navbar-burger").toggleClass("is-active");
-         $(".navbar-menu").slideToggle();
-
+$(document).ready(function () {
+   $(".navbar-burger").click(function () {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").slideToggle();
    });
 
-   if($(window).width() < 1000) {
-      $('.has-dropdown .navbar-link').click(function(e) {
+   if ($(window).width() < 1000) {
+      $('.has-dropdown .navbar-link').on('click', function (e) {
          $(this).siblings('.navbar-dropdown').slideToggle();
          e.preventDefault();
+         e.stopPropagation();
+         console.log('clicked');
       });
    }
-   
-   $(window).resize(function() {
-      if($(window).width() < 1000) {
-         $('.has-dropdown .navbar-link').click(function(e) {
+
+   $(window).on('resize', function () {
+      if ($(window).width() < 1000) {
+         $('.has-dropdown .navbar-link').on('click', function (e) {
             $(this).siblings('.navbar-dropdown').slideToggle();
             e.preventDefault();
+            e.stopPropagation();
          });
       }
    });
 });
 
 // Animate Story
-$(document).ready(function() {
+$(document).ready(function () {
    $('.story-text:even').addClass('slideInRight');
    $('.story-text:odd').addClass('slideInLeft');
 });
 
 
 // Remove inline width & heights
-$(document).ready(function($){
-   $('img').each(function(){
-   $(this).removeAttr('width')
-   $(this).removeAttr('height');
-  });
+$(document).ready(function ($) {
+   $('img').each(function () {
+      $(this).removeAttr('width')
+      $(this).removeAttr('height');
+   });
 });
 
 // Init Wow
 new WOW().init();
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-   if($(window).width() > 1000) {
+   if ($(window).width() > 1000) {
       $('.advantages-img').attr('data-wow-offset', '300');
    }
 
-   $(window).resize(function() {
-      if($(window).width() > 1000) {
+   $(window).resize(function () {
+      if ($(window).width() > 1000) {
          $('.advantages-img').attr('data-wow-offset', '300');
       } else $('.advantages-img').removeAttr('data-wow-offset', '300');
    });
